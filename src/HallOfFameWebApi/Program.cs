532767b997108d708dbb467665576ec75d8ed3e2
Alexander Katkov <afargoth@gmail.com>
2024-01-21 20:49:05 +0700

    Add solution and project

---
+++ src/HallOfFameWebApi/Program.cs
@@ -0,0 +1,17 @@
+namespace HallOfFameWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                {
+                    webHostBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/HallOfFameWebApi/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace HallOfFameWebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
