42875a97558eea5683a1663df4652a3c44114699
Alexander Katkov <afargoth@gmail.com>
2024-01-25 10:33:36 +0700

    Add exception filter

--- src/HallOfFameWebApi/Controllers/PersonController.cs
+++ src/HallOfFameWebApi/Controllers/PersonController.cs
@@ -1,40 +1,28 @@
 ﻿using Asp.Versioning;
 using HallOfFameWebApi.Entities;
+using HallOfFameWebApi.Filters;
 using HallOfFameWebApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 namespace HallOfFameWebApi.Controllers
 {
     [Route("api/v{version:apiVersion}/persons")]
     [ApiController]
     [ApiVersion("1.0")]
+    [TypeFilter<HandleExceptionFilter>]
     public class PersonController : ControllerBase
     {
         private readonly IPersonService _service;
-        private readonly ILogger<PersonController> _logger;
 
-        public PersonController(IPersonService service, ILogger<PersonController> logger)
+        public PersonController(IPersonService service)
         {
             _service = service;
-            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetPersons()
         {
-            IEnumerable<Person> persons;
-
-            try
-            {
-                persons = await _service.GetPersons();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get persons");
-                return StatusCode((int)HttpStatusCode.InternalServerError);
-            }
-
+            IEnumerable<Person> persons = await _service.GetPersons();
             return Ok(persons);
         }
     }
---
+++ src/HallOfFameWebApi/Filters/HandleExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Net.Mime;
+
+namespace HallOfFameWebApi.Filters
+{
+    public class HandleExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostEnvironment _environment;
+
+        public HandleExceptionFilter(IHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            bool isDevelopment = _environment.IsDevelopment();
+            var details = new ProblemDetails
+            {
+                Status = (int)HttpStatusCode.InternalServerError,
+                Title = isDevelopment
+                    ? context.Exception.Message
+                    : "Something went wrong",
+                Detail = isDevelopment
+                    ? context.Exception.StackTrace
+                    : null
+            };
+            context.Result = new ObjectResult(details)
+            {
+                StatusCode = details.Status,
+                ContentTypes = [MediaTypeNames.Application.ProblemJson]
+            };
+        }
+    }
+}
