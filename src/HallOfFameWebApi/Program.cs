48585779347d3ba7b044f0a1a202942aedfd3d86
Alexander Katkov <afargoth@gmail.com>
2024-01-28 16:02:05 +0700

    Add serilog
    Write logs to file

--- src/HallOfFameWebApi/Program.cs
+++ src/HallOfFameWebApi/Program.cs
@@ -1,17 +1,57 @@
+using HallOfFameWebApi.Configuration;
+using Serilog;
+
 namespace HallOfFameWebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog(ConfigureSerilog)
                 .ConfigureWebHostDefaults(webHostBuilder =>
                 {
                     webHostBuilder.UseStartup<Startup>();
                 });
+
+        public static void ConfigureSerilog(HostBuilderContext context, LoggerConfiguration config)
+        {
+            config.ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            var loggingOptions = context.Configuration
+                .GetSection("FileLogging").Get<FileLoggingOptions>();
+
+            if (loggingOptions?.Enable == true)
+            {
+                config.WriteTo.File(
+                    path: loggingOptions.Path,
+                    rollingInterval: loggingOptions.RollingInterval,
+                    outputTemplate: loggingOptions.OutputTemplate,
+                    fileSizeLimitBytes: loggingOptions.FileSizeLimitBytes,
+                    retainedFileCountLimit: loggingOptions.RetainedFileCountLimit
+                );
+            }
+        }
     }
 }
